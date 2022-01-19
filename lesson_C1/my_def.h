b24b82bc97bbd8780ef63198b99c7d7c1e6c544e
bernadelli <clariton.bernadelli@uftm.edu.br>
2022-01-19 16:41:59 -0300

    First version

---
+++ lesson_C1/my_def.h
@@ -0,0 +1,29 @@
+/*
+ * Lesson B3: GPIO Interrupt
+ * Prof. Dr. Cláriton Bernadelli
+ * Electrical engineering - Federal University of Triângulo Mineiro – UFTM
+ * Signal processing group - GPS - UFTM - Brazil
+ * Version jan/2021  - github.com/bernadelli/pds
+ */
+
+#ifndef MY_DEF_H_
+#define MY_DEF_H_
+
+//---PORT-F I/O---//
+#define PF0 0x01
+#define PF1 0x02
+#define PF2 0x04
+#define PF3 0x08
+#define PF4 0x10
+
+
+//---USER FUNCTION'S---//
+void SetSystemClock(unsigned int);
+void Time1A_1sec_delay(void);
+
+
+
+
+
+#endif /* MY_DEF_H_ */
+
